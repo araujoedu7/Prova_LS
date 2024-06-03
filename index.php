fde7a9649343debcf9ff4a2bcf465248c800a2f7
araujoedu7 <ea144027@gmail.com>
2024-06-03 10:12:06 -0300

    modificação no index e no cadastro

--- index.php
+++ index.php
@@ -10,10 +10,35 @@
 
 <body bg-body-tertiary>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
     <div class="container mt-5">
         <div class="row aling-items-center">
             <div class="col-md-10 mx-auto col-lg-5 ">
+            <p class="h2">Faça Login!</p>
+            <form action="index.php" method="POST">
+        <input type="hidden" name="acao" value="cadastrar">
+        <div class="mb-3">
+            <label>E-mail</label>
+            <input type="email" name="email" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label>Senha</label>
+            <input type="password" name="senha" class="form-control">
+        </div>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-success">Entrar</button>
+            <p>Não tem uma conta? <a class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="novo-usuario.php">Entrar</a> </p>
+        </div>
+
+    </form>
+               
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="container mt-5">
+        <div class="row aling-items-center">
+            <div class="col-md-10 mx-auto col-lg-5 ">
+            <p class="h2">Faça Login</p>
                 <form action="index.php" class="p-4 p-md-5 border rounder-3 bg-light border rounded" >
                     <div class="form-floater">
                         <label for="loginName">Email</label>
@@ -24,17 +49,12 @@
                         <label for="loginSenha">Senha</label>
                         <input type="password" class="form-control" name="senha" id="loginPassword" placeholder="Senha">
                     </div>
-
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" value="lembrar-me"> Lembrar-me
-                        </label>
-                    </div>
-                     <button class="btn btn-lg btn-success" type="submit">Entrar</button>
+                     <button class="btn btn-lg btn-success mt-3" type="submit">Entrar</button>
+                     <div class="mt-2"><a class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="novo-usuario.php">Não tem uma conta? Crie uma.</a></div>
                 </form>
             </div>
         </div>
-    </div>
+    </div> -->
 
 </body>
 <?php
--- novo-usuario.php
+++ novo-usuario.php
@@ -1,20 +1,25 @@
 <?php 
-    session_start();
-    require_once ('config.php');
+    // session_start();
+    // require_once ('config.php');
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
+<!doctype html>
+<html lang="pt-br" data-bs-theme="light">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Faça seu  Cadastro!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
-    <h1>Novo Usuários</h1>
+<body bg-body-tertiary>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-    <form action="?page-salvar" method="POST">
+    <div class="container mt-5">
+        <div class="row aling-items-center">
+            <div class="col-md-10 mx-auto col-lg-5 ">
+            <p class="h2">Cadastre-se</p>
+            <form action="?page-salvar" method="POST">
         <input type="hidden" name="acao" value="cadastrar">
         <div class="mb-3">
             <label>Nome</label>
@@ -29,41 +34,45 @@
             <input type="password" name="senha" class="form-control">
         </div>
         <div class="mb-3">
-            <button type="submit" class="btn btn-primary">Enviar</button>
+            <button type="submit" class="btn btn-success">Enviar</button>
+            <p>Já tem uma conta? <a class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="index.php">Entrar</a> </p>
         </div>
 
     </form>
-</body>
+               
+            </div>
+        </div>
+    </div>
 
-</html>
+</body>
 
 <?php
 
 
 
-session_start();
-require_once('config.php');
+// session_start();
+// require_once('config.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['nome'], $_POST['email'], $_POST['senha'])) {
-        $nomeUser = mysqli_real_escape_string($conn, $_POST['nome']);
-        $emailUser = mysqli_real_escape_string($conn, $_POST['email']);
-        $senhaUser = mysqli_real_escape_string($conn, $_POST['senha']);
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     if (isset($_POST['nome'], $_POST['email'], $_POST['senha'])) {
+//         $nomeUser = mysqli_real_escape_string($conn, $_POST['nome']);
+//         $emailUser = mysqli_real_escape_string($conn, $_POST['email']);
+//         $senhaUser = mysqli_real_escape_string($conn, $_POST['senha']);
 
-        // Consulta SQL usando prepared statement
-        $sql = "INSERT INTO tabela_usuarios (nomeUser, emailUser, senhaUser) VALUES ('nomeUser','emailUser' , 'senhaUser')";
-        $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, 'sss', $nomeUser, $emailUser, $senhaUser);
+//         // Consulta SQL usando prepared statement
+//         $sql = "INSERT INTO tabela_usuarios (nomeUser, emailUser, senhaUser) VALUES ('nomeUser','emailUser' , 'senhaUser')";
+//         $stmt = mysqli_prepare($conn, $sql);
+//         mysqli_stmt_bind_param($stmt, 'sss', $nomeUser, $emailUser, $senhaUser);
 
-        if (mysqli_stmt_execute($stmt)) {
-            echo 'Cadastrado com sucesso!';
-        } else {
-            echo 'Erro na inserção: ' . mysqli_error($conn);
-        }
+//         if (mysqli_stmt_execute($stmt)) {
+//             echo 'Cadastrado com sucesso!';
+//         } else {
+//             echo 'Erro na inserção: ' . mysqli_error($conn);
+//         }
 
-        mysqli_stmt_close($stmt);
-    }
-}
+//         mysqli_stmt_close($stmt);
+//     }
+// }
 
 // $nomeUser = $_POST['nome'];
 // $emailUser = $_POST['email'];
@@ -77,3 +86,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 // }
 ?>
+
